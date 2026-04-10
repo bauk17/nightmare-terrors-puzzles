18986a69081f7dcd7d22e8425d9d29c2e3fc5d65
Caua Pereira <cauapereira_@hotmail.com>
2026-04-10 19:01:14 -0300

    chore: setting up deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: 'export',
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Essencial para o GitHub Pages
+  images: {
+    unoptimized: true, // Necessário porque o GitHub Pages não suporta a otimização de imagem padrão do Next
+  },
 };
 
 export default nextConfig;
